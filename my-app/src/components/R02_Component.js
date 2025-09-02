5426292628ef2a23602ad073212b389dae23efd2
Jihoon <kgh5079855@gmail.com>
2025-09-02 16:15:45 +0900

    React수업

---
+++ my-app/src/components/R02_Component.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+// 컴포넌트는 각각의 독립된 UI 조각 개념이다.(재사용)
+// 종류는 함수형,클래스형
+// 컴포넌트에는 state,props가 있다.
+// - state: 컴포넌트 자신이 관리하는 데이터
+// - props: 부모컴포넌트가 자식컴포넌트에게 전달하는 데이터
+
+// rsf: 항수형 컴포넌트 기본형
+// - 함수작성 방식으로 구현
+// - 기존에 클래스형에서 사용하는 state, lifecycle 기능 X
+function R02_Component(props) {
+    return (
+        <div>
+                <h2>함수형 컴포넌트</h2>
+        </div>
+    );
+}
+
+//rcc: 클래스형 컴포넌트
+// - component를 상속받아 구현
+// - render() 메서드 를 사용해서 렌더링을 구현
+
+class R02_ComponentClass extends Component {
+    render() {
+        return (
+            <div>
+                <h2>클래스형 컴포넌트</h2>
+            </div>
+        );
+    }
+}
+//export는 다른 컴포넌트에서 사용할 수 있도록 설정하는 방식
+//default는 기본으로 반드시 작성
+export default R02_Component;
+//여러 컴포넌트를 추가할 수 있다.
+export {R02_ComponentClass};
---
+++ my-app/src/components/R03_Props.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+//props: 부모컴포넌트 -> 자식컴포넌트로 전달하는 데이터
+//   - props는 읽기전용(immutable) -> 자식컴포넌트에서 props값 변경 불가
+//   - 구조분해할당을 사용하면 가독성에 좋음
+
+// 부모컴포넌트에서 props로 name:kang, age:25 전달
+function R03_Props(props) {
+    //const name=props.name;
+    //const age=props.age;
+    const {name, age}=props; //구조분해 할당
+    return (
+        <div>
+            <p>안녕하세요, <strong>{name}</strong>({age}세)님!</p>
+        </div>
+    );
+}
+
+function R03_Props2({name, age}) {
+    return (
+        <div>
+            <div>{name},{age}</div>
+        </div>
+    );
+}
+
+function R03_Props3(props) {
+    return (
+        <div>
+            <div>{props.name},{props.age}</div>
+        </div>
+    );
+}
+
+export default R03_Props;
+export {R03_Props2};
+export {R03_Props3};
