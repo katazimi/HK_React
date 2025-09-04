7ac7e8181a6f315b846da84e3f3c675d6cdcdd8b
Jihoon <kgh5079855@gmail.com>
2025-09-04 09:13:21 +0900

    리액트를 다루는 기술(Book)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Counter from './book/Counter';
+import MyComponent from './book/MyComponent';
 import R01_JSXBasic from './components/R01_JSXBasic';
 import R02_Component, { R02_ComponentClass } from './components/R02_Component';
 import R03_Props, { R03_Props2, R03_Props3, R03_PropsClass } from './components/R03_Props';
 import R04_State, { R04_StateClass } from './components/R04_State';
 import R05_Event from './components/R05_Event';
 import R06_Loop_list from './components/R06_Loop_list';
+
 
 function App() {
   //바벨: jsx문법을 가능하게 해주는 기능제공
@@ -52,7 +55,6 @@ function App() {
         <h1>6.Loop 사용하기</h1>
         <R06_Loop_list />
       </div>
-
     </div>
   );
 }
